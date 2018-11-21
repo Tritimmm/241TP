0e72bf95f2440d256b45e5be6009155bd94e55cc
Tritimmm <maggiezhangjiayi@gmail.com>
2018-11-20 23:43:07 -0500

    initial commit

---
+++ simpleImplementation_Maggie.py
@@ -0,0 +1,17 @@
+#jiayizha
+
+#turn a list of links into a matrix
+#the i,j entry of which represents the connection between links i,j
+
+def connections(links):
+    for 
+
+
+
+
+
+
+#test cases
+links1=[[]]
+print("Testing connections()")
+assert(connections(links1)==

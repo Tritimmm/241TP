0c4595b90cbb8fc649c0b23a5777f54a4bc9cd61
Tritimmm <maggiezhangjiayi@gmail.com>
2018-11-23 14:09:55 -0500

    diag M finished

--- PMatrix_Maggie.py
+++ PMatrix_Maggie.py
@@ -1,10 +1,11 @@
 #jiayizha
 
-#turn a list of links into a matrix
-#the i,j entry of which represents the connection between links i,j
-
 import numpy as np
 
+#turn a list of links into a matrix
+#the j,i entry of which represents the connection between links i,j
+#returns initial matrix M with stochastic cols
+#(I'm terrible at naming things, so change it if you like)
 def connections(links):
     result = [[0]*len(links) for i in range(len(links))]
     for i in range(len(links)): #use index as write-up
@@ -17,8 +18,14 @@ def connections(links):
             pass
     return result    
 
+#diagonalize M
+def diagM(M): #question: will P always be invertible?
+    eigval , P = np.linalg.eig(M)
+    D=np.diag(list(eigval))
+    Pinv=np.linalg.inv(P)
+    return [P,D,Pinv]
 
-#test cases
+#Test cases
 links1 = [[1,2,3],[3],[0],[1,2]]
 print("Testing connections()")
 assert(connections(links1) == [[  0,   0,   1,   0],
@@ -26,3 +33,7 @@ assert(connections(links1) == [[  0,   0,   1,   0],
                                [1/3,   0,   0, 1/2],
                                [1/3,   1,   0,   0]])
 print("Success.")
+
+print(diagM(connections(links1)))
+
+
---
+++ SideNotes.py
@@ -0,0 +1,5 @@
+#Important Theorems:
+    Perron–Frobenius Theorem
+#Eigenstuff:
+    we want to be able to apply the probablity matrix M over and over again, so we diagnalize it. (M is always gonna be a sqr matrix, so we can do this).
+

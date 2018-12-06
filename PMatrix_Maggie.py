a845cf5ef7b37eba7e48c4d9a1989acd862e6039
18satoy <30479857+18satoy@users.noreply.github.com>
2018-12-06 09:44:32 -0500

    added convergence

--- PMatrix_Maggie.py
+++ PMatrix_Maggie.py
@@ -28,12 +28,55 @@ def diagM(M): #question: will P always be invertible?
 #Test cases
 links1 = [[1,2,3],[3],[0],[1,2]]
 print("Testing connections()")
-assert(connections(links1) == [[  0,   0,   1,   0],
-                               [1/3,   0,   0, 1/2],
-                               [1/3,   0,   0, 1/2],
-                               [1/3,   1,   0,   0]])
+assert(connections(links1) == [[  0,   0,     1,   0],
+                               [1/3,   0,     0, 1/2],
+                               [1/3,   0,     0, 1/2],
+                               [1/3,   1,     0,   0]])
 print("Success.")
 
-print(diagM(connections(links1)))
 
+def convergence(diag):
+    P, D, Pinv = diag[0], diag[1], diag[2]
+    print("D:", np.linalg.matrix_power(D, 80))
+    N = len(D)
+    x0 = np.array([1 for i in range(N)])
+    x1 = P @ np.linalg.matrix_power(D, 80) @ Pinv @ x0
+    # error = [0.001 for i in range(N)]
+    # vector = converge(P, D, Pinv, x0, x1)
+    return x1
+    
+x = [[1, 5], [2, 5], [1, 3, 5], [4], [1, 5], [2, 6], [0, 1]]
+    
+print(convergence(diagM(connections(x))))
+    
+    
+def converge(P, D, Pinv, x0, x1, depth=1):
+    if depth == 800:
+        print("x1", x1)
+        return x1
+    else:
+        # x1 = P @ np.linalg.matrix_power(D, depth) @ Pinv @ x0
+        x0 = x1
+        x1 = P @ D @ Pinv @ x0
+        converge(P, D, Pinv, x0, x1, depth + 1)
 
+# print(convergence(connections(links1)))
+
+A = [[  0,   0,     1,   0],
+                               [1/3,   0,     0, 1/2],
+                               [1/3,   0,     0, 1/2],
+                               [1/3,   1,     0,   0]]
+
+N = len(A)
+x0 = np.array([1/N for i in range(N)])
+x1 = A @ x0
+def simple(A, x0, x1, depth=1):
+    if depth == 10:
+        print(x1)
+        return x1
+    else:
+        x0 = x1
+        x1 = A @ x0
+        simple(A, x0, x1, depth + 1)
+    
+# print(simple(A, x0, x1))
